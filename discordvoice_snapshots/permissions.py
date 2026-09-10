40dd85995724b4800d376cd9ae1c84250cd5607f
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 15:05:51 -0500

    Update permissions for snapshot access

--- discordvoice_snapshots/permissions.py
+++ discordvoice_snapshots/permissions.py
@@ -2,5 +2,5 @@ from allianceauth.permissions import site_admin_required
 from django.contrib.auth.decorators import permission_required
 
 admin_required = site_admin_required
-editor_required = permission_required("snapshots.change_snapshot")
-viewer_required = permission_required("snapshots.view_snapshot")
+editor_required = permission_required("discordvoice_snapshots.change_snapshot")
+viewer_required = permission_required("discordvoice_snapshots.view_snapshot")
