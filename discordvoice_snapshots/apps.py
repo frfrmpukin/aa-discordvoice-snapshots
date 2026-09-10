faf7ee3dba8e929f4da1b641261cd0e8c9b6ff7c
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 12:47:15 -0500

    Add Django AppConfig for Discord Voice Snapshots

--- discordvoice_snapshots/apps.py
+++ discordvoice_snapshots/apps.py
@@ -1,1 +1,6 @@
+from django.apps import AppConfig
 
+class DiscordVoiceSnapshotsConfig(AppConfig):
+    name = "discordvoice_snapshots"
+    label = "discordvoice_snapshots"
+    verbose_name = "Discord Voice Snapshots"
