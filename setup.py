0c74045ca07b59273c733b495de247d68bbe58aa
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 12:48:01 -0500

    Add setup.py for package configuration

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="aa-discordvoice-snapshots",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "allianceauth>=5.2.0",
+    ],
+)
