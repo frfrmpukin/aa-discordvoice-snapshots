4dc99321a3a29967c121e84a1e872d392767384d
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-10 13:29:24 -0500

    Add permission decorators for snapshots

--- discordvoice_snapshots/permissions.py
+++ discordvoice_snapshots/permissions.py
@@ -1,1 +1,6 @@
+from allianceauth.permissions import site_admin_required
+from django.contrib.auth.decorators import permission_required
 
+admin_required = site_admin_required
+editor_required = permission_required("snapshots.change_snapshot")
+viewer_required = permission_required("snapshots.view_snapshot")
