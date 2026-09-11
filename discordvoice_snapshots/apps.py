8c27cc5a1faffa3cd1c42d38af76f04be6b41e6a
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 10:33:47 -0500

    Refactor DiscordVoiceSnapshotConfig class
    Removed redundant DiscordVoiceSnapshotsConfig class.

--- discordvoice_snapshots/apps.py
+++ discordvoice_snapshots/apps.py
@@ -4,8 +4,3 @@ from . import __version__
 class DiscordVoiceSnapshotConfig(AppConfig):
     name = "aa_discord_voicesnapshot"
     verbose_name = f"Discord Voice Snapshot ({__version__})"
-    
-class DiscordVoiceSnapshotsConfig(AppConfig):
-    name = "discordvoice_snapshots"
-    label = "discordvoice_snapshots"
-    verbose_name = "Discord Voice Snapshots"
