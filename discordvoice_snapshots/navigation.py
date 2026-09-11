19f099856e175ac69386a8d686950ebd1bb1fd8d
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 02:28:34 -0500

    Add VoiceSnapshotMenu for Discord voice snapshots

---
+++ discordvoice_snapshots/navigation.py
@@ -0,0 +1,19 @@
+from allianceauth.services.hooks import MenuItemHook
+from allianceauth import hooks
+
+class VoiceSnapshotMenu(MenuItemHook):
+    def __init__(self):
+        super().__init__(
+            "Discord Voice Snapshots",
+            "fa fa-microphone",
+            "discordvoice_snapshots:list",
+        )
+
+    def render(self, request):
+        if request.user.has_perm("aa_discord_voicesnapshot.view_snapshot_history"):
+            return super().render(request)
+        return ""
+
+@hooks.register('menu_item_hook')
+def register_menu():
+    return VoiceSnapshotMenu()
