ab6f097ed3416df48336b1a5a0b6922e43bab835
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 09:45:32 -0500

    Modify setup.py for package details and requirements
    Updated package name, version, and dependencies in setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="aa-discordvoice-snapshots",
-    version="0.1.0",
+    name="aa-discord-voicesnapshot",
+    version="1.0.0",
+    description="Alliance Auth module for recording and managing Discord voice channel snapshots.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/YOUR_GITHUB/aa-discord-voicesnapshot",
+    project_urls={
+        "Source": "https://github.com/frfrmpukin/aa-discord-voicesnapshot",
+        "Tracker": "https://github.com/frfrmpukin/aa-discord-voicesnapshot/issues",
+    },
+
     packages=find_packages(),
     include_package_data=True,
+
     install_requires=[
-        "allianceauth>=5.2.0",
+        "allianceauth>=3.0.0",
+        "django>=3.2",
     ],
+
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+    python_requires=">=3.8",
 )
