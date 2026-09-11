0b627611b260424280df49f39a39f2d189b9a63e
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 10:31:15 -0500

    Add versioning to Discord Voice Snapshot app config

--- discordvoice_snapshots/apps.py
+++ discordvoice_snapshots/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from . import __version__
 
+class DiscordVoiceSnapshotConfig(AppConfig):
+    name = "aa_discord_voicesnapshot"
+    verbose_name = f"Discord Voice Snapshot ({__version__})"
+    
 class DiscordVoiceSnapshotsConfig(AppConfig):
     name = "discordvoice_snapshots"
     label = "discordvoice_snapshots"
