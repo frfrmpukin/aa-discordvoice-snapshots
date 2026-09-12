7dcc13b4b5a5217addb61c3c7e7289dc4b80ab26
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 23:37:37 -0500

    Remove docstring from log_action function
    Removed docstring from log_action function.

--- discordvoice_snapshots/utils.py
+++ discordvoice_snapshots/utils.py
@@ -1,13 +1,8 @@
 from django.utils import timezone
 from .models import AuditLog
 
+
 def log_action(user, action, old_value=None, new_value=None):
-    """
-    Creates an audit log entry.
-    user: AA User or None
-    action: string describing the action
-    old_value/new_value: optional text fields
-    """
     AuditLog.objects.create(
         user=user,
         action=action,
