4f010b9f22b30804232d8293c56bfd6eae6f8644
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 23:34:37 -0500

    Rename app config class and update name and verbose name

--- discordvoice_snapshots/apps.py
+++ discordvoice_snapshots/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 from . import __version__
 
-class DiscordVoiceSnapshotConfig(AppConfig):
-    name = "aa_discord_voicesnapshot"
-    verbose_name = f"Discord Voice Snapshot ({__version__})"
+class DiscordVoiceSnapshotsConfig(AppConfig):
+    name = "discordvoice_snapshots"
+    verbose_name = f"Discord Voice Snapshots ({__version__})"
