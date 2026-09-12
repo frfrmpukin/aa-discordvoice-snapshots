7aed42cd5362ac25178384f02ef397def81fac76
FrFrmPukin <frfrmpukin@multiplexgaming.com>
2026-09-11 23:36:34 -0500

    Update permission decorators to raise exceptions

--- discordvoice_snapshots/permissions.py
+++ discordvoice_snapshots/permissions.py
@@ -2,5 +2,13 @@ from allianceauth.permissions import site_admin_required
 from django.contrib.auth.decorators import permission_required
 
 admin_required = site_admin_required
-editor_required = permission_required("discordvoice_snapshots.change_snapshot")
-viewer_required = permission_required("discordvoice_snapshots.view_snapshot")
+
+editor_required = permission_required(
+    "discordvoice_snapshots.change_snapshot",
+    raise_exception=True,
+)
+
+viewer_required = permission_required(
+    "discordvoice_snapshots.view_snapshot",
+    raise_exception=True,
+)
